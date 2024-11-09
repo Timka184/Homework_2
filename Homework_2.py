9f3f56b8e2416a32cab2cfcda06c0d03d1c41c38
Timur <takhmeto2009@gmail.com>
2024-11-09 14:02:19 +0500

    Add files via upload

---
+++ Homework_2.py
@@ -0,0 +1,5 @@
+Course_name = 'Python'
+Number_of_homework_completed = 12
+Number_of_hours_spent = 1.5
+Time_per_task = Number_of_hours_spent / Number_of_homework_completed
+print("Курс:" + Course_name + ','" всего задач:" + str(Number_of_homework_completed) + ','" затрачено часов:" + str(Number_of_hours_spent) + ','" среднее время выполнения:" + str(Time_per_task))
